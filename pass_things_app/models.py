ac9323706f7aa27bcb01a9c73b5024afd0ebb06c
Rafal <noahh.r@gmail.com>
2021-02-24 19:34:44 +0100

    model institution

--- pass_things_app/models.py
+++ pass_things_app/models.py
@@ -6,3 +6,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Institution(models.Model):
+    TYPE_CHOICES = (
+        (1, 'fundacja'),
+        (2, 'organizacja pozarządowa'),
+        (3, 'zbiórka lokalna'),
+    )
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, default=1)
+    categories = models.ManyToManyField('Category')
