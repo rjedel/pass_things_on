ea7cd1d1cf55cd498ff6ecb3563f28d6125f20a2
Rafal <noahh.r@gmail.com>
2021-02-24 20:44:36 +0100

    model donation

--- pass_things_app/models.py
+++ pass_things_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -18,3 +19,17 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, default=1)
     categories = models.ManyToManyField('Category')
+
+
+class Donation(models.Model):
+    quantity = models.PositiveSmallIntegerField()
+    categories = models.ManyToManyField('Category')
+    institution = models.ForeignKey('Institution', on_delete=models.PROTECT, related_name='donations')
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=40)
+    zip_code = models.CharField(max_length=5)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=255, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None, related_name='donations')
