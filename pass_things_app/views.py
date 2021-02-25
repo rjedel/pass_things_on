d9a6419a747d49528f48d875a3dbd7ab3770c3c5
Rafal <noahh.r@gmail.com>
2021-02-25 21:03:15 +0100

    landing page - dynamic institution load

--- pass_things_app/views.py
+++ pass_things_app/views.py
@@ -2,16 +2,32 @@ from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
 
-from .models import Donation
+from .models import Donation, Institution
 
 
 class LandingPageView(View):
     def get(self, request):
         total_quantity = Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
         supported_institutions = Donation.objects.values('institution').distinct().count()
+
+        foundations = Institution.objects.filter(type=Institution.TYPE_CHOICES[0][0]).order_by('?')[:3]
+        for foundation in foundations:
+            foundation.categories_str = ', '.join(cat.name for cat in foundation.categories.order_by('name')[:5])
+
+        ngos = Institution.objects.filter(type=Institution.TYPE_CHOICES[1][0]).order_by('?')[:4]
+        for ngo in ngos:
+            ngo.categories_str = ', '.join(cat.name for cat in ngo.categories.order_by('name')[:5])
+
+        local_donations = Institution.objects.filter(type=Institution.TYPE_CHOICES[2][0]).order_by('?')[:2]
+        for ld in local_donations:
+            ld.categories_str = ', '.join(cat.name for cat in ld.categories.order_by('name')[:5])
+
         ctx = {
             'total_quantity': total_quantity,
             'supported_institutions': supported_institutions,
+            'foundations': foundations,
+            'ngos': ngos,
+            'local_donations': local_donations,
         }
         return render(request, 'pass_things_app/index.html', context=ctx)
 
