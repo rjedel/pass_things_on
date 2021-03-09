6a0c26a97ba8fe050ed49a4081a91eaed9b3e171
Rafal <noahh.r@gmail.com>
2021-03-09 23:29:12 +0100

    fix forms labels

--- pass_things_app/forms.py
+++ pass_things_app/forms.py
@@ -5,11 +5,11 @@ from .models import Category, Institution
 
 
 class RegisterForm(forms.Form):
-    name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Imię'}))
-    surname = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}))
-    email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}))
+    name = forms.CharField(label=False, max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Imię'}))
+    surname = forms.CharField(label=False, max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}))
+    email = forms.EmailField(label=False, max_length=254, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(label=False, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
+    password2 = forms.CharField(label=False, widget=forms.PasswordInput(attrs={'placeholder': 'Powtórz hasło'}))
 
     def clean(self):
         cleaned_data = super(RegisterForm, self).clean()
@@ -21,8 +21,8 @@ class RegisterForm(forms.Form):
 
 
 class CustomLoginForm(forms.Form):
-    email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
+    email = forms.EmailField(label=False, max_length=254, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(label=False, widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
 
     def clean(self):
         cleaned_data = super().clean()
--- pass_things_app/static/pass_things_app/js/register_login.js
+++ pass_things_app/static/pass_things_app/js/register_login.js
@@ -1,4 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('label').forEach(el => el.style.display = 'None');
     document.querySelectorAll('p input').forEach(el => el.parentElement.className = "form-group");
 });
